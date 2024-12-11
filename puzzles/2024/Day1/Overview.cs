b14b554d670c06e723acab03b6e160d16b5b01ef
tacosontitan <contact@tacosontitan.com>
2024-12-10 19:02:23 -0600

    Port AoC from Hussy

---
+++ puzzles/2024/Day1/Overview.cs
@@ -0,0 +1,15 @@
+﻿namespace Aoc24;
+
+/// <summary>
+/// Defines implementations and tests for day one of Advent of Code in 2024.
+/// </summary>
+public sealed partial class DayOne
+{
+    /// <summary>
+    ///     Determines the distance between the two specified lists as defined in the problem
+    ///     statement for day one's challenge.
+    /// </summary>
+    public delegate int Solution(
+        IEnumerable<int> leftList,
+        IEnumerable<int> rightList);
+}
